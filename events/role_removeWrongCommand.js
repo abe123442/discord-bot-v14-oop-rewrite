8779f3f8aca47025eb640fd38da5ad209e32ef5e
Chromaggus <harryzhang3939@gmail.com>
2022-10-26 13:26:19 +1100

    Implemented the role_removeWrongCommand event

--- events/role_removeWrongCommand.js
+++ events/role_removeWrongCommand.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const COURSE_CHATS_CHANNEL_ID = 860388285511630868;
 
 module.exports = {
     name: "messageCreate",
     async execute(message) {
         try {
 
-            if (message.content.startsWith("/role give") || message.content.startsWith("/role remove")) {
+            if (message.content.includes("/role") /* &&  message.channelId ==  COURSE_CHATS_CHANNEL_ID*/) {
                 
-                console.log(message.channel, message.channelId);
+                // console.log(message.channelId);
                 const msg = "Role command entered incorrectly. Please see the above messages on how to correctly give or remove a role."
-    
+                
+                // Send message and then delete it shortly afterwards
+                // Can't send ephemeral messages though...
                 await message.reply({ content: msg, ephemeral: true })
                     .then(msg => {
                         setTimeout(() => msg.delete(), 5000)
