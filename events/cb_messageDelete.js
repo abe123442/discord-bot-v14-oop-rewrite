bbc7cc9a7b921ede64ea7b0deea58afb88ce6520
sschreyer <samschreyer360@gmail.com>
2022-11-09 16:32:51 +1100

    carrotboard: fix cb_messageDelete breaking

--- events/cb_messageDelete.js
+++ events/cb_messageDelete.js
@@ -9,20 +9,14 @@ module.exports = {
     /**
      * @param {Message} message 
      */
-    // TODO: seems to be a bug where if the bot crashes/turns off, and you try 
-    // to delete an old msg that was sent when this bot was running previously,
-    // it will crash the bot??? No clue why/how to fix.
     async execute(message) {
-        // check if partial
-        // if (message.partial) {
-        //     message = await message.fetch();
-        // }
-
         /** @type {CarrotboardStorage} */
         const cbStorage = global.cbStorage;
 
         const channelID = message.channelId;
         const serverId = message.guild.id;
+
+        console.log(message);
         
         // remove it from storage, and update leaderboard
         await cbStorage.db.del_entry(message.id, message.channelId);    
