6a170b6a2a851756b9028863f1f38254c414c962
abe123442 <ae248abraham@gmail.com>
2023-02-10 08:13:52 +1100

    Refactored the deploy command script and added a clean command to package.json

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,42 +1,50 @@
-import { APIApplicationCommand, REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
-import config from './config.js';
-import { SlashCommand } from './types.js';
-import { join } from 'node:path';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { readdirSync } from 'node:fs';
+import { APIApplicationCommand, REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js'
+import config from './config.js'
+import { SlashCommand } from './types.js'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { readdirSync } from 'node:fs'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const commandsDir = join(__dirname, 'commands')
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = readdirSync(commandsDir).filter(file => file.endsWith('.js'));
+const getCommands = async (commandsDir: string) => {
+	const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = []
+	// Grab all the command files from the commands directory
+	const commandFiles = readdirSync(commandsDir).filter(file => file.endsWith('.js'))
 
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(config.token);
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-(async () => {
 	for (const file of commandFiles) {
 		const filePath = join(commandsDir, file)
 		const ImportedCommand = (await import(filePath)).default as SlashCommand
-		// console.log(ImportedCommand)
-		commands.push(ImportedCommand.command.toJSON());
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+		commands.push(ImportedCommand.command.toJSON())
 	}
+	return commands
+}
 
-	// deploy your commands!
+const deployCommands = async (commands: RESTPostAPIChatInputApplicationCommandsJSONBody[]) => {
+	// Construct and prepare an instance of the REST module
+	const rest = new REST({ version: '10' }).setToken(config.token)
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(config.clientId), { body: commands }
 		) as APIApplicationCommand[]
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`)
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
+		console.error(error)
 	}
-})();
+}
+
+const getAndDeployCommands = async (commandsDir: string) => {
+	getCommands(commandsDir)
+		.then(commands => deployCommands(commands))
+		.catch(error => console.error(error))
+}
+
+const commandsDir = join(__dirname, 'commands')
+getAndDeployCommands(commandsDir)
+
+export default getAndDeployCommands
